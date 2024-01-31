ac8fab4b7166405a141e66957f703b73de525fe2
lulzxz <zakyarifrahman83@gmail.com>
2024-02-01 05:36:35 +0700

    modified:   src/components/LoginForm.tsx 	modified:   src/components/RegisterForm.tsx 	modified:   src/components/apacik.css

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,44 +1,46 @@
 'use client'
 import { redirect } from 'next/dist/server/api-utils';
 import React, { Component } from 'react';
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { SyntheticEvent, useState } from "react";
 
 
 const LoginForm = () => {
   const router = useRouter();
+//   const router = useRouter();
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+//   const [email, setEmail] = useState('')
+//   const [password, setPassword] = useState('')
 
-  const [alert, setAlert] = useState(false)
-  const handleLogin = async(e:SyntheticEvent) => {
-    e.preventDefault();
+//   const [alert, setAlert] = useState(false)
+//   const handleLogin = async(e:SyntheticEvent) => {
+//     e.preventDefault();
 
-    const signInData = await signIn('credentials', {
-      email: email,
-      password: password,
-      redirect: false,
-  })
+//     const signInData = await signIn('credentials', {
+//       email: email,
+//       password: password,
+//       redirect: false,
+//   })
 
-  if(signInData?.error){
-    setAlert(true)
-  }else {
-    setAlert(false)
-  }
+//   if(signInData?.error){
+//     setAlert(true)
+//   }else {
+//     setAlert(false);
+//     router.push('/');
+//   }
 
-  setEmail("")
-    setPassword("")
-}
+//   setEmail("")
+//     setPassword("")
+// }
 
     return (
     <div className="form-container sign-in-container">
     <img src='/assets/logo2 2.png' alt="logo" width={180} className='p-4'/>
-    <form className="form-signin" onSubmit={handleLogin}>
+    <form className="form-signin">
         <h1 className="form-title-singin">Sign In to InCube!</h1>
        
-        {alert &&
+        {/* {alert &&
         <div role="alert" className="alert alert-error fixed bottom-10 right-10 max-w-96 self">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -55,12 +57,12 @@ const LoginForm = () => {
           </svg>
           <span className="text-white">Email atau password salah</span>
         </div>
-    }
+    } */}
 
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
 
-       <button type='submit' className="form-button">Sign In</button>
+       <button type='button' onClick={()=> router.push('/')} className="form-button">Sign In</button>
         <a className="find-password" href="#">Forget Password</a>
     </form>
 </div>
