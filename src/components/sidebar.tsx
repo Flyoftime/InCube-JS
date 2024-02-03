d4fb351bbf7f9d2dc05999124eff04ec9976e708
lulzxz <zakyarifrahman83@gmail.com>
2024-02-04 02:25:04 +0700

    third v2

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -7,13 +7,13 @@ const Sidebar = () => {
     <div className='flex'>
         
         <div className='bg-[#FFB800] h-screen p-5 pt-8 w-52'>
-         <div className="avatar">
+         <div className="avatar object-center relative left-8">
             <div className="w-24 rounded-full">
                 <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
             </div>
         </div>
-        <div className='grid gap-[20px]'>
-            <div className='flex justify-normal gap-1'>
+        <div className='grid gap-[20px] object-center top-20 left-5 relative'>
+            <div className='flex justify-normal gap-1 '>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />         
             </svg> 
