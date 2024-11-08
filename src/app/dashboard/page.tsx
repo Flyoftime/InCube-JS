1d96365f149e444082eab3a21ded6676bc464bc7
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-11-08 08:48:29 +0700

    feat: add id user in session

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,24 +1,23 @@
-import Dashboard from '@/components/dashboard';
-import Sidebar from '@/components/sidebar';
-import React from 'react';
-import { useSession } from 'next-auth/react';
-import { redirect, useRouter } from 'next/navigation';
-import { getServerSession } from 'next-auth';
-import { authOptionts } from '../api/auth/[...nextauth]/route';
+import Dashboard from "@/components/dashboard";
+import Sidebar from "@/components/sidebar";
+import React from "react";
+import { useSession } from "next-auth/react";
+import { redirect, useRouter } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptionts } from "../api/auth/[...nextauth]/route";
 
 const Dashboardpage = async () => {
-const session = await getServerSession(authOptionts);
-if (session) {
-  return (
-    <div className='flex'>
-      <Sidebar />
-      <Dashboard />
-    </div>
-  );
-} else {
-  redirect("/login");
-}
-  
-}
+  const session = await getServerSession(authOptionts);
+  if (session) {
+    return (
+      <div className="flex">
+        <Sidebar />
+        <Dashboard />
+      </div>
+    );
+  } else {
+    redirect("/login");
+  }
+};
 
 export default Dashboardpage;
